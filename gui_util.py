b759a93fbc90c1191a9257905f69b9ee36337151
Konsantin Stepanyuk <k.stepanyuk@intabia.ru>
2024-09-09 13:45:47 +0200

    added click_on_image_and_hold

--- gui_util.py
+++ gui_util.py
@@ -34,6 +34,40 @@ def click_on_image(image_path, confidence=0.9, max_processing_sec=3, wait_time=3
     return False
 
 
+def click_on_image_and_hold(image_path, confidence=0.9, max_processing_sec=3, wait_time=3, hold_time=1):
+    """
+    Находит заданную картинку на экране и кликает по ней.
+
+    :param image_path: Путь к файлу картинки
+    :param confidence: Уверенность в совпадении (по умолчанию 0.9)
+    :param max_processing_sec: Время ожидания в секундах (по умолчанию 10)
+    """
+    start_time = time.time()
+    while time.time() - start_time < max_processing_sec:
+        try:
+            # Поиск картинки на экране
+            location = pyautogui.locateOnScreen(image_path, confidence=confidence)
+            if location is not None:
+                # Получение центра найденной области
+                center = pyautogui.center(location)
+                # Клик по центру найденной области
+                logging.info(f"Зажали картинку {image_path} выполнен.")
+                pyautogui.mouseDown(center)
+                time.sleep(hold_time)
+                pyautogui.mouseUp(center)
+
+                time.sleep(wait_time)
+                return True
+        except Exception as e:
+            logging.error(f"Ошибка при поиске картинки: {e}")
+
+        time.sleep(1)
+
+    logging.warning(f"Картинка {image_path} не найдена в течение {max_processing_sec} секунд.")
+    return False
+
+
+
 def is_image_existed_on_screen(image_path, confidence=0.9, max_processing_sec=3):
     """
     Проверяет наличие заданной картинки на экране.
