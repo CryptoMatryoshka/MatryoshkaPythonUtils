4b5c57ce20c066aab7d1c8c36b44b1ff4a5d6072
Konsantin Stepanyuk <k.stepanyuk@intabia.ru>
2024-09-13 14:02:02 +0200

    add backup account feature

--- gui_util.py
+++ gui_util.py
@@ -17,13 +17,6 @@ def scroll_down_by_dragdrop(shift_px):
 
 
 def click_on_image(image_path, confidence=0.9, max_processing_sec=3, wait_time=3):
-    """
-    Находит заданную картинку на экране и кликает по ней.
-
-    :param image_path: Путь к файлу картинки
-    :param confidence: Уверенность в совпадении (по умолчанию 0.9)
-    :param max_processing_sec: Время ожидания в секундах (по умолчанию 10)
-    """
     start_time = time.time()
     while time.time() - start_time < max_processing_sec:
         try:
@@ -45,6 +38,30 @@ def click_on_image(image_path, confidence=0.9, max_processing_sec=3, wait_time=3
 
     logging.warning(f"Картинка {image_path} не найдена в течение {max_processing_sec} секунд.")
     return False
+
+
+def move_to_image(image_path, confidence=0.9, max_processing_sec=3, wait_time=3):
+    start_time = time.time()
+    while time.time() - start_time < max_processing_sec:
+        try:
+            # Поиск картинки на экране
+            location = pyautogui.locateOnScreen(image_path, confidence=confidence)
+            if location is not None:
+                # Получение центра найденной области
+                center = pyautogui.center(location)
+                # Клик по центру найденной области
+                pyautogui.moveTo(center)
+                logging.info(f"Навели мыш на картинку {image_path}.")
+
+                time.sleep(wait_time)
+                return location
+        except Exception as e:
+            logging.error(f"Ошибка при поиске картинки: {e}")
+
+        time.sleep(1)
+
+    logging.warning(f"Картинка {image_path} не найдена в течение {max_processing_sec} секунд.")
+    return None
 
 
 def click_on_image_and_hold(image_path, confidence=0.9, max_processing_sec=3, wait_time=3, hold_time=1):
@@ -78,7 +95,6 @@ def click_on_image_and_hold(image_path, confidence=0.9, max_processing_sec=3, wa
 
     logging.warning(f"Картинка {image_path} не найдена в течение {max_processing_sec} секунд.")
     return False
-
 
 
 def is_image_existed_on_screen(image_path, confidence=0.9, max_processing_sec=3):
